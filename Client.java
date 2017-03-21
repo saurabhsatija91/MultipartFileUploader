ebf9680cb4b6657e5362ede2fcd24e7ad4e46ecd
Saurabh Satija <saurabh.satija91@gmail.com>
2017-03-20 21:19:08 -0700

    Adding Client worker thread.
    Signed-off-by: Saurabh Satija <saurabh.satija91@gmail.com>

--- Client.java
+++ Client.java
@@ -6,50 +6,17 @@ import java.io.RandomAccessFile;
 import java.net.Socket;
 
 public class Client implements Runnable {
-	protected Socket clientSocket = null;
-	protected int clientID = 0;
-	private byte[] messageBytes;
-	
-	private static final int metaSize = 4;
+	private int port;
+	private int numClients;
 
-	private static String opFileName = "/Users/Saurabh/Documents/DataTempTransfer/client/newFile0";
-	
-	public Client (int port, int ID) {
-		try {
-			this.clientSocket = new Socket ("localhost", port);
-		} catch (IOException e) {
-			System.err.println("Server can not be reached.");
-			return;
-		}
-		this.clientID = ID;
+	public Client(int port, int numClients) {
+		this.port = port;
+		this.numClients = numClients;
 	}
-	
-	public void run() {
-		try {
-			InputStream is = clientSocket.getInputStream();
-			RandomAccessFile raf = new RandomAccessFile(opFileName + ".txt", "rw");
 
-			byte[] offArr = new byte[metaSize];
-			byte[] partSize = new byte[metaSize];
-			is.read(offArr, 0, metaSize);
-			is.read(partSize, 0, metaSize);
-			
-			int offset = Integer.parseInt((new String(offArr)).trim());
-			int part = Integer.parseInt((new String(partSize)).trim());
-			
-			messageBytes = new byte[part];
-			is.read(messageBytes);
-			
-			raf.seek(offset);
-			System.out.println("Client " + this.clientID + " seeked to " + offset);
-			raf.write(messageBytes, 0, part);
-			raf.close();
-			is.close();
-			
-			System.out.println("Client " + clientID + ": Finished Reading and Writing.");
-		} catch (IOException e) {
-			e.printStackTrace();
+	public void run() {
+		for (int i = 0; i < numClients; i++) {
+			new Thread(new WorkerClient(i + 1, port)).start();
 		}
-		
 	}
 }
--- Tester.java
+++ Tester.java
@@ -4,26 +4,23 @@ public class Tester {
 	public static void main(String[] args) {
 		ServerMultiThreaded newServer = new ServerMultiThreaded(4445);
 		new Thread(newServer).start();
-		
+
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
-		Client client1 = new Client(4445, 1);
-		Client client2 = new Client(4445, 2);
-		Client client3 = new Client(4445, 3);
-		
-		new Thread(client1).start();
-		new Thread(client2).start();
-		new Thread(client3).start();
+
+		Client newClient = new Client(4445, 3);
+
+		new Thread(newClient).start();
+
 		try {
 			Thread.sleep(10000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
+
 		System.out.println("Stopping server.");
 		newServer.stop();
 	}
